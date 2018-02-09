bbdb2634751aca492eee99752c3a37d28aefe20e
Ilya Dolinskiy <dolinsky94@yandex.ru>
2018-02-09 19:42:01 +0500

    /

---
+++ frontend/src/main/webapp/js/Message.js
@@ -0,0 +1,113 @@
+Messages = Class.extend({
+    handler: {},
+
+    init: function () {
+        this.handler['Pawn'] = this.handlePawn;
+        this.handler['Bomb'] = this.handleBomb;
+        this.handler['Wood'] = this.handleTile;
+        this.handler['Wall'] = this.handleTile;
+        this.handler['Grass'] = this.handleTile;
+        this.handler['Fire'] = this.handleFire;
+    },
+
+    move: function (direction) {
+        var template = {
+            topic: "MOVE",
+            data: {}
+        };
+
+        template.data.direction = direction.toUpperCase();
+        template.data.possess = gInputEngine.possessed;
+        return JSON.stringify(template);
+    },
+
+    plantBomb: function () {
+        var template = {
+            topic: "PLANT_BOMB",
+            data: gInputEngine.possessed
+        };
+
+        return JSON.stringify(template);
+    },
+
+
+    handleReplica: function (msg) {
+        var gameObjects = JSON.parse(msg.data).objects;
+        var survivors = new Set();
+
+        for (var i = 0; i < gameObjects.length; i++) {
+            var obj = gameObjects[i];
+            if (gMessages.handler[obj.type] === undefined)
+                continue;
+
+            survivors.add(obj.id);
+            gMessages.handler[obj.type](obj);
+        }
+        gGameEngine.gc(survivors);
+    },
+
+    handlePossess: function (msg) {
+        gInputEngine.possessed = parseInt(msg.data);
+    },
+
+    handlePawn: function(obj) {
+        var player = gGameEngine.players.find(function (el) {
+            return el.id === obj.id;
+        });
+        var position = Utils.getEntityPosition(obj.position);
+
+        if (player) {
+            player.bmp.x = position.x;
+            player.bmp.y = position.y;
+        } else {
+            console.log(new Date().getTime() + " handel new player " + obj.id);
+            player = new Player(obj.id, position);
+            gGameEngine.players.push(player);
+        }
+    },
+
+    handleBomb: function(obj) {
+        var bomb = gGameEngine.bombs.find(function (el) {
+            return el.id === obj.id;
+        });
+        var position = {};
+        position.x = obj.position.x + 6;
+        position.y = -obj.position.y + 12 * 33 - 6;
+        if (bomb) {
+            bomb.bmp.x = position.x;
+            bomb.bmp.y = position.y;
+        } else {
+            bomb = new Bomb(obj.id, position);
+            gGameEngine.bombs.push(bomb);
+        }
+    },
+
+    handleTile: function (obj) {
+        var tile = gGameEngine.tiles.find(function (el) {
+            return el.id === obj.id;
+        });
+
+        var position = Utils.getEntityPosition(obj.position);
+        if (tile) {
+            tile.material = obj.type;
+        } else {
+            tile = new Tile(obj.id, obj.type, position);
+            gGameEngine.tiles.push(tile);
+        }
+    },
+
+    handleFire: function (obj) {
+        var fire = gGameEngine.fires.find(function (el) {
+            return el.id === obj.id;
+        });
+
+        var position = Utils.getEntityPosition(obj.position);
+        if (!fire) {
+            fire = new Fire(obj.id, position);
+            gGameEngine.fires.push(fire);
+        }
+    }
+
+});
+
+gMessages = new Messages();
---
+++ frontend/src/main/webapp/js/ServerProxy.js
@@ -0,0 +1,188 @@
+ServerProxy = Class.extend({
+    gameServerUrl: "localhost:8091",
+    matchMakerJoinUrl: "http://localhost:8080/matchmaker/join",
+    matchMakerSignUpUrl: "http://localhost:8080/matchmaker/signup",
+    matchMakerSignOutUrl: "http://localhost:8080/matchmaker/signout",
+    matchMakerTopUrl: "http://localhost:8080/matchmaker/top",
+    matchMakerOnlineListUrl: "http://localhost:8080/matchmaker/onlinelist",
+    gameId: "1234",
+    currentState: "offline",
+
+    socket: null,
+
+    handler: {},
+
+    init: function () {
+        this.handler['REPLICA'] = gMessages.handleReplica;
+        this.handler['POSSESS'] = gMessages.handlePossess;
+    },
+
+    subscribeEvents: function () {
+        var self = this;
+        gInputEngine.subscribe('up', function () {
+            self.socket.send(gMessages.move('up'))
+        });
+        gInputEngine.subscribe('down', function () {
+            self.socket.send(gMessages.move('down'))
+        });
+        gInputEngine.subscribe('left', function () {
+            self.socket.send(gMessages.move('left'))
+        });
+        gInputEngine.subscribe('right', function () {
+            self.socket.send(gMessages.move('right'))
+        });
+        gInputEngine.subscribe('bomb', function () {
+            self.socket.send(gMessages.plantBomb())
+        });
+    },
+
+    registryFromMatchMaker: function (login, password) {
+        var that = this;
+        var msg = "";
+        var succ = false;
+        $.ajax({
+            contentType: 'application/x-www-form-urlencoded',
+            data: "login=" + login + "&password=" + password,
+            dataType: 'text',
+            success: function (data) {
+                msg = data;
+                succ = true;
+                console.log("Matchmaker registry");
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                msg = jqXHR.responseText;
+                console.log(jqXHR.responseText);
+            },
+            processData: false,
+            type: 'POST',
+            url: that.matchMakerSignUpUrl,
+            async: false
+        });
+        return [succ, msg];
+    },
+
+    getSessionIdFromMatchMaker: function (login, password) {
+        var that = this;
+        var msg = "";
+        var succ = false;
+        $.ajax({
+            contentType: 'application/x-www-form-urlencoded',
+            data: "login=" + login + "&password=" + password,
+            dataType: 'text',
+            success: function (data) {
+                that.gameId = data;
+                succ = true;
+                console.log("Matchmaker returned gameId=" + data);
+                that.connectToGameServer(that.gameId, login);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                msg = jqXHR.responseText;
+                console.log(jqXHR.responseText);
+                console.log(errorThrown);
+            },
+            processData: false,
+            type: 'POST',
+            url: that.matchMakerJoinUrl
+        });
+        return [succ, msg];
+    },
+
+    connectToGameServer: function (gameId, login) {
+        var self = this;
+        this.socket = new WebSocket("ws://" + this.gameServerUrl + "/game/connect?gameId=" + gameId + "&name=" + login);
+
+        this.socket.onopen = function () {
+            console.log("Connection established.");
+        };
+
+        this.socket.onclose = function (event) {
+            if (event.wasClean) {
+                console.log('closed');
+            } else {
+                console.log('alert close');
+            }
+            console.log('Code: ' + event.code + ' cause: ' + event.reason);
+        };
+
+        this.socket.onmessage = function (event) {
+            var msg = JSON.parse(event.data);
+            if (self.handler[msg.topic] === undefined)
+                return;
+
+            self.handler[msg.topic](msg);
+        };
+
+        this.socket.onerror = function (error) {
+            console.log("Error " + error.message);
+        };
+    },
+
+    logout: function (login, password) {
+        var that = this;
+        $.ajax({
+            contentType: 'application/x-www-form-urlencoded',
+            data: "login=" + login + "&password=" + password,
+            dataType: 'text',
+            success: function (data) {
+                console.log("Matchmaker logout");
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR.responseText);
+            },
+            type: 'POST',
+            url: that.matchMakerSignOutUrl,
+            async: false
+        });
+    },
+
+    TopPlayers: function () {
+        var that = this;
+        $.ajax({
+            contentType: 'application/x-www-form-urlencoded',
+            success: function (response) {
+                console.log("Matchmaker get Top");
+                var players = response.split(", ");
+                if (players != "") {
+                    for (var i = 0; i < players.length; i++) {
+                        var player = players[i].split("=");
+                        document.write('<tr><th>' + (i + 1) +
+                            '</th><td>' + player[0] +
+                            '</td><td>' + player[1] + '</td></tr>');
+                    }
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR.responseText);
+            },
+            type: 'GET',
+            url: that.matchMakerTopUrl,
+            async: false
+        });
+    },
+
+    OnlinePlayers: function () {
+        var that = this;
+        $.ajax({
+            contentType: 'application/x-www-form-urlencoded',
+            success: function (response) {
+                console.log("Matchmaker get Online players");
+                var players = response.split(", ");
+                if (players != "") {
+                    for (var i = 0; i < players.length; i++) {
+                        document.write('<tr><th>' + (i + 1) + '</th><td>' + players[i] + '</td></tr>');
+                    }
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR.responseText);
+            },
+            type: 'GET',
+            url: that.matchMakerOnlineListUrl,
+            async: false
+        });
+    }
+
+
+});
+
+serverProxy = new ServerProxy();
